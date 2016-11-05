a5256e5c2dd9b932ab54ae70cc68153137d698d1
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-04 19:31:50 -0500

    initial commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,28 @@
+var count = 0;
+var studentNumber = 0;
+
 $(document).ready(function(){
     $.ajax({
       type: "GET",
       url: "/data",
       success: function(data){
-        console.log(data);
+        appendDom(data);
       }
     });
 });
+
+function appendDom(data) {
+  for (var i = 0 ; i < data.sigmanauts.length ; i++) {
+    $("#students").append('<div class="indivStudent'+ count + '"></div>');
+//style="display:none"
+    //setting var $el to individual student divs
+    var $el = $("#students").children().last();
+    $el.append('<h3>' + data.sigmanauts[i].name + '</h3>');
+    $el.append('<h5>' + "Github Username: " + data.sigmanauts[i].git_username + '</h5>');
+    $el.append('<p>' + "Shoutout: " + data.sigmanauts[i].shoutout + '</p>');
+
+    $("#counter").append('<div class="studentCounter'+ count + '"></div>');
+
+    count++;
+  }
+}
